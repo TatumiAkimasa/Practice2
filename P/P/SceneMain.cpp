4d739d4f8ec12b537a970739ea8c1bca28239bb2
晃聖 辰巳 <h1202019035@hiratagakuen.onmicrosoft.com>
2020-10-14 17:21:22 +0900

    ファイル入れました。

---
+++ P/P/SceneMain.cpp
@@ -0,0 +1,41 @@
+//STLデバック機能をOffにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GmameLで使用するヘッダー
+#include "GameL\DrawTexture.h"
+#include "GameL\SceneObjManager.h"
+#include "GameL\Audio.h"
+
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "SceneMain.h"
+#include "GameHead.h"
+
+
+//コンストラクタ
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+
+}
+
+//ゲームメイン初期化メソッド
+void CSceneMain::InitScene()
+{
+
+}
+
+//ゲーム実行中メソッド
+void CSceneMain::Scene()
+{
+
+}
